e520c1d3793d2b002f6bf9db1ea9507ddda81469
shortivorytower <shortivorytower@gmail.com>
2017-07-11 00:57:33 +0800

    fixed DP

--- python/compute_state_value_function.py
+++ python/compute_state_value_function.py
@@ -1,4 +1,5 @@
 state_prob_dict = {
+    0: {'l': 0, 'r': 1, 'u': 0, 'd': 4},
     1: {'l': 0, 'r': 2, 'u': 1, 'd': 5},
     2: {'l': 1, 'r': 3, 'u': 2, 'd': 6},
     3: {'l': 2, 'r': 3, 'u': 3, 'd': 7},
@@ -13,25 +14,16 @@ state_prob_dict = {
     12: {'l': 12, 'r': 13, 'u': 8, 'd': 12},
     13: {'l': 12, 'r': 14, 'u': 9, 'd': 13},
     14: {'l': 13, 'r': 15, 'u': 10, 'd': 14},
+    15: {'l': 14, 'r': 15, 'u': 11, 'd': 15}
 }
 
 
 def prob(new_state, reward, current_state, action):
-    # terminate state
-    if current_state == 1 and action=='l' and new_state == 0 and reward == 0:
-        return 1.0
-    if current_state == 4 and action=='u' and new_state == 0 and reward == 0:
-        return 1.0
-    if current_state == 11 and action=='d' and new_state == 15 and reward == 0:
-        return 1.0
-    if current_state == 14 and action=='r' and new_state == 15 and reward == 0:
-        return 1.0
-
     if reward != -1:
-        return 0.0
+        raise Exception('Expecting all reward value to be -1')
 
     if not (current_state in state_prob_dict):
-        return 0.0
+        raise Exception('Unexpected current_state value ' + str(current_state))
 
     cur_state_dict = state_prob_dict[current_state]
     if not (action in cur_state_dict):
@@ -47,38 +39,43 @@ def prob(new_state, reward, current_state, action):
 if __name__ == '__main__':
 
     # given ANY state, it is always four direction left, right, up, down even they are at the edge.
-    pi_val = 1.0/4.0
+    # so the probability of any direction is 1/4
+    pi_val = 0.25
     gamma = 1.0
 
     # state from 1 to 14
     max_state = 15
     # mapping from state to value
     V_dict = {}
-    for i in range(1, max_state):
+    for i in range(0, max_state + 1):
         V_dict[i] = 0.0
 
-    epsilon = 0.00000001
-    iteration_count = 0
+    theta = 0.0000001
+    k = 0
     while True:
         delta = 0.0
+        new_dict = {0: 0.0, max_state: 0.0}
         for s in range(1, max_state):
             v = V_dict[s]
-            vs = 0.0
+            new_vs = 0.0
             for a in ['l', 'r', 'u', 'd']:
                 temp = 0.0
-                for new_state in range(1, max_state):
+                for new_state in range(0, max_state + 1):
                     reward = -1
                     temp = temp + prob(new_state, reward, s, a) * (reward + gamma * V_dict[new_state])
-                vs = vs + pi_val * temp
-            V_dict[s] = vs
+                new_vs = new_vs + pi_val * temp
 
-            delta = max(delta, abs(v - vs))
+            new_dict[s] = new_vs
+            delta = max(delta, abs(v - new_vs))
 
-        iteration_count += 1
-        print 'iterated', iteration_count
-        if delta < epsilon:
+        V_dict = new_dict
+
+        k += 1
+        if k % 50 == 0:
+            print 'iterated', k
+        if delta < theta:
             break
 
-    print 'Finished'
-    for s in range(1, max_state):
+    print 'Iterative Policy Evaluation Result for grid world:'
+    for s in range(0, max_state + 1):
         print 'state', s, ':', V_dict[s]
