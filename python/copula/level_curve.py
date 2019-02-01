e8c7a9899ec41aefd226ae3bd3550ee1cc72741e
pc <pc@pc.com>
2019-02-01 17:34:46 +0800

    efadfasdd

--- python/copula/level_curve.py
+++ python/copula/level_curve.py
@@ -19,7 +19,7 @@ def level_curve(theta, u, t):
 if __name__ == '__main__':
     theta = 3.0
     u_list = np.arange(0.01, 1, 0.01)
-    t = 0.4
+    t = 0.6
     v_list = level_curve(theta, u_list, t)
     print(u_list)
     print(v_list)
