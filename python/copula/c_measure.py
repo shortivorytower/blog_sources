ed06b361ac63997a60235631d7f5dd6fe9fdb752
shortivorytower <shortivorytower@gmail.com>
2019-02-02 17:03:25 +0800

    added frank copula testing

--- python/copula/c_measure.py
+++ python/copula/c_measure.py
@@ -30,30 +30,28 @@ def level_curve(generator_func, inverted_generator_func, u, t):
     v = inverted_generator_func(generator_func(t) - generator_func(u))
     return v
 
-
-
 if __name__ == '__main__':
 
     theta = 3.0
-    t = 0.35
+    t = 0.33
     print('theta = {0}, t = {1},  Theoretical Clayton C-Measure = {2}'.format(theta, t, clayton_c_measure(theta, t)))
 
     clayton_generator = lambda t: np.power(t, -theta) - 1
     clayton_inverted_generator = lambda s: np.power(1.0 + s, -1.0 / theta)
 
-    sim_count = 100000
+    np.random.seed(1)
+    sim_count = 500000
     sim_result = generate_clayton_copula_2d(theta, sim_count)
     u_list = sim_result[0]
     v_list = sim_result[1]
+    clayton_level_curve = level_curve(clayton_generator, clayton_inverted_generator, u_list, t)
 
-    level_curve_v = level_curve(clayton_generator, clayton_inverted_generator, u_list, t)
-    #print(u_list)
-    #print(v_list)
-    #print(level_curve_v)
-    in_range_count = 0
-    for v, level_v in zip(v_list, level_curve_v):
-        if v <= level_v or np.isnan(level_v) or level_v < 0:
-            in_range_count += 1
+    # set all negative values and NA to 1
+    # so that we accept any simulated v values (which must be between 0 and 1)
+    clayton_level_curve[np.isnan(clayton_level_curve)] = 1.0
+    clayton_level_curve[clayton_level_curve < 0 ] = 1.0
 
-    print('C(u,v)<={0} : count={1}'.format(t,in_range_count))
+    in_range_count = (v_list<=clayton_level_curve).sum()
+
+    print('C(u,v)<={0} : hit count={1} out of {2}'.format(t, in_range_count, sim_count))
     print('Simulated C-Measure = {0}'.format(in_range_count / sim_count))
