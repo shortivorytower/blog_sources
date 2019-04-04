c037476d34f20cfa9297a965b56215067ce07f82
shortivorytower <shortivorytower@gmail.com>
2019-04-05 01:01:09 +0800

    sdfdsasfd

--- python/x_sigma_rho/x_sigma_rho_decomp.py
+++ python/x_sigma_rho/x_sigma_rho_decomp.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import statsmodels.api as sm
 from math import sqrt
 
 if __name__ == '__main__':
@@ -27,7 +28,7 @@ if __name__ == '__main__':
     xau_df = xau_df[1:]
 
 
-    all_returns_df = pd.DataFrame({
+    daily_returns_df = pd.DataFrame({
         'Date' : hsi_df['Date'],
         'HSBC_return' : hsbc_df['return'],
         'Zijin_Mining_return' : zijin_mining_df['return'],
@@ -37,17 +38,37 @@ if __name__ == '__main__':
     }, columns = ['Date', 'HSBC_return', 'Zijin_Mining_return', 'HSI_return', 'SSE_Comp_return', 'XAU_return'])
 
     # annualized return SD
-    hsbc_return_sd = all_returns_df['HSBC_return'].std() * sqrt(252)
-    zijin_mining_return_sd = all_returns_df['Zijin_Mining_return'].std() * sqrt(252)
+    hsbc_return_sd = daily_returns_df['HSBC_return'].std() * sqrt(252)
+    zijin_mining_return_sd = daily_returns_df['Zijin_Mining_return'].std() * sqrt(252)
 
     # factors SD
-    hsi_return_sd = all_returns_df['HSI_return'].std() * sqrt(252)
-    sse_composite_return_sd = all_returns_df['SSE_Comp_return'].std() * sqrt(252)
-    xau_return_sd = all_returns_df['XAU_return'].std() * sqrt(252)
+    hsi_return_sd = daily_returns_df['HSI_return'].std() * sqrt(252)
+    sse_composite_return_sd = daily_returns_df['SSE_Comp_return'].std() * sqrt(252)
+    xau_return_sd = daily_returns_df['XAU_return'].std() * sqrt(252)
 
-    #print(all_returns_df.corr())
+    factors_correlations = daily_returns_df.drop(['Date', 'HSBC_return', 'Zijin_Mining_return'], axis=1).corr()
+    #print(factors_correlations)
 
-    factors_correlations = all_returns_df.drop(['Date', 'HSBC_return', 'Zijin_Mining_return'], axis=1).corr()
+    hsbc_model = sm.OLS(daily_returns_df['HSBC_return'], daily_returns_df[['HSI_return', 'SSE_Comp_return', 'XAU_return']]).fit()
+    hsbc_hsi_exposure = hsbc_model.params['HSI_return']
+    hsbc_sse_exposure = hsbc_model.params['SSE_Comp_return']
+    hsbc_xau_exposure = hsbc_model.params['XAU_return']
+    hsbc_specific_risk = hsbc_model.resid.std()*sqrt(252)
 
-    print(factors_correlations)
+    print('HSBC Exposure on HSI = {0}, SSE Comp = {1}, XAU = {2}'.format(hsbc_hsi_exposure, hsbc_sse_exposure, hsbc_xau_exposure))
+    print('HSBC Specific Risk', hsbc_specific_risk)
+    #print(hsbc_model.summary())
+
+
+    zijin_model = sm.OLS(daily_returns_df['Zijin_Mining_return'], daily_returns_df[['HSI_return', 'SSE_Comp_return', 'XAU_return']]).fit()
+    zijin_hsi_exposure = zijin_model.params['HSI_return']
+    zijin_sse_exposure = zijin_model.params['SSE_Comp_return']
+    zijin_xau_exposure = zijin_model.params['XAU_return']
+    zijin_specific_risk = zijin_model.resid.std()*sqrt(252)
+
+    print('Zijin Mining Exposure on HSI = {0}, SSE Comp = {1}, XAU = {2}'.format(zijin_hsi_exposure, zijin_sse_exposure, zijin_xau_exposure))
+    print('Zijin Mining Specific Risk', zijin_specific_risk)
+    #print(zijin_model.summary())
+
+
     print('done')
