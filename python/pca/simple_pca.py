3b50181ce2efccfd11443421083a816a763aaea3
shortivorytower <shortivorytower@gmail.com>
2020-10-21 01:51:29 +0800

    updated variables

--- python/pca/simple_pca.py
+++ python/pca/simple_pca.py
@@ -9,8 +9,6 @@ p=10
 np.random.seed(1)
 
 X = np.random.normal(5.0, 1.0, size=(p, n))
-#X = np.random.poisson(20, size=(p, n))
-#X = np.random.geometric(0.5, size=(p, n))
 
 # mean center the observation
 bar_X = X.mean(axis=0)
@@ -21,7 +19,6 @@ Y = X-bar_X
 # But using Y is easier to understand geomatrically.
 C = np.cov(Y.transpose())
 
-
 # eigendecomosition
 eig_vals, U = eig(C)
 Lambda = np.diag(eig_vals)
@@ -30,11 +27,14 @@ Lambda = np.diag(eig_vals)
 # NOTE: when we are running huge amount of real life data points, this can really be unmatched!!!
 print('Covariance can be reconstructed:', np.all(np.isclose(C-U.dot(Lambda).dot(U.transpose()), 0)))
 
-# Projection of Y data points into Principal Components
-Z = Y.dot(U)
+# the explained variance of each principal components
+print('Explained Variance:', eig_vals/np.sum(eig_vals))
+
+# Projection of Y data points into Principal Components (i.e. scores)
+W = Y.dot(U)
 
 # Reconstruction of the original data
-Reconstructed_X = Z.dot(U.transpose()) + bar_X
+X_new = W.dot(U.transpose()) + bar_X
 
 # Check data reconstruction result
-print('Reconstructed data matched:', np.all(np.isclose(X - Reconstructed_X,0)))
+print('Reconstructed data matched:', np.all(np.isclose(X - X_new,0)))
